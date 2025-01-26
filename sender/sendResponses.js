a9f3c4ca164a87a59946afc74cfb63075e670386
saadullahkhan12 <muhmmadsaadullah093@gmail.com>
2025-01-26 08:28:16 +0500

    aa

--- sender/sendResponses.js
+++ sender/sendResponses.js
@@ -1,7 +1,7 @@
 function sendResponses(res, status, data, error, message) {
   return res.status(status).json({
       success: !error,
-      message: message || (error ? 'Error is occurred' : 'Success'),
+      message: message || (error ? 'Erro is occurred' : 'Success'),
       data: data || null,
       error: error || null
   });
